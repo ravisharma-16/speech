2f19982e2e1e837c6f4fe33a7fcb78dd81219e1a
RAVI <raviisharmaa.2004@gmail.com>
2024-11-02 17:39:37 +0530

    Add files via upload

---
+++ speech.js
@@ -0,0 +1,47 @@
+// let speech = new SpeechSynthesisUtterance();
+// let voices = [];
+// let voiceSelect = document.querySelector("select");
+
+// window.speechSynthesis.onvoiceschanged = () =>
+// {
+//     voices = window.speechSynthesis.getVoices();
+//     speech.voice = voices[0];
+//     voices.forEach((voice,i) => (voiceSelect.options[i] = new option(voice.name,i)));
+// };
+
+
+
+// document.querySelector("button").addEventListener("click",() =>{
+//     speech.text = document.querySelector("textarea").value;
+//     window.speechSynthesis.speak(speech);
+// });
+
+
+
+
+
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+
+    voiceSelect.innerHTML = "";
+
+    voices.forEach((voice, i) => {
+        let option = new Option(voice.name, i);
+        voiceSelect.appendChild(option);
+    });
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
